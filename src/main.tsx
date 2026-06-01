5eb32a2f175b21cc25e33cedad26c0234ea05e0d
Faraz <farazmirza1110@gmail.com>
2026-06-01 11:20:59 +0530

    Switch to HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/skins/circle.css";
 import "@/styles/globals.css";
@@ -10,8 +10,8 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
